32aca213e43e48e8515cbc167eecf7d9c1039e8e
Samir Batista Bezerra Torres Júnior <samir_batista.junior@somosicev.com>
2024-06-25 22:35:37 -0300

    Carregando dataset e dividindo dados em conjutos de treinamento e teste.

--- ConversorAudioTexto/treinamento.py
+++ ConversorAudioTexto/treinamento.py
@@ -6,3 +6,8 @@ from sklearn.metrics import classification_report
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
+recognizer = sr.Recognizer()
+data = pd.read_csv('2019-05-28_portuguese_hate_speech_hierarchical_classification.csv')
+X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=42)
+
+
