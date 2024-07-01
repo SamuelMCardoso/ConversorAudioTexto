11c3cf751ccc00401f3a423f51d2e11031820874
Samir Batista Bezerra Torres Júnior <samir_batista.junior@somosicev.com>
2024-06-30 22:56:35 -0300

    modificando código para reconhecer o caminho fornecido.

--- ConversorAudioTexto/treinamento.py
+++ ConversorAudioTexto/treinamento.py
@@ -1,26 +1,31 @@
-from io import BytesIO
-
-import requests
 import speech_recognition as sr
 import pandas as pd
+import requests
+from io import BytesIO
+from pydub import AudioSegment
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline, make_pipeline
-
-
-class AudioSegment:
-    @classmethod
-    def from_mp3(cls, audio_data):
-        pass
+import os
 
 
 def main():
-
     recognizer = sr.Recognizer()
-    csv_path= r"C:\Users\samir\OneDrive\Documents\Python\2019-05-28_portuguese_hate_speech_hierarchical_classification.csv"
+    # Use uma string bruta para o caminho do arquivo CSV
+    csv_path = r'C:\Users\samir\OneDrive\Documents\Python\frases.csv'
     data = pd.read_csv(csv_path)
+
+    # Imprime as colunas e as primeiras linhas do DataFrame
+    print("Colunas do DataFrame:", data.columns)
+    print("Primeiras linhas do DataFrame:\n", data.head())
+
+    # Verifica se as colunas 'text' e 'label' existem
+    if 'text' not in data.columns or 'label' not in data.columns:
+        print("Erro: O DataFrame não contém as colunas 'text' e/ou 'label'")
+        return
+
     X_train, X_test, y_train, y_test = train_test_split(data['text'], data['label'], test_size=0.2, random_state=42)
 
     model = make_pipeline(TfidfVectorizer(), MultinomialNB())
@@ -46,18 +51,15 @@ def main():
         audio = recognizer.record(source)
 
     try:
-
-
-
-       text = recognizer.recognize_google(audio, language='pt-BR')
-       print(f"Transcrição: {text}")
-       resultado_filtrado = filtrar_frases_pejorativas(text)
-       print("Resultado final: {resultado_filtrado}")
+        text = recognizer.recognize_google(audio, language='pt-BR')
+        print(f"Transcrição: {text}")
+        resultado_filtrado = filtrar_frases_pejorativas(text)
+        print(f"Resultado final: {resultado_filtrado}")
     except sr.UnknownValueError:
-       print("Não foi possível entender o áudio.")
+        print("Não foi possível entender o áudio.")
     except sr.RequestError:
-       print("Erro ao se comunicar com o serviço de reconecimento da fala.")
+        print("Erro ao se comunicar com o serviço de reconhecimento da fala.")
+
 
 if __name__ == '__main__':
-  main()
-
+    main()
